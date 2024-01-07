9dce79224c13223af0cd9c87c907131fb9cc1325
Muhammad Moiz <97216206+Moiz005@users.noreply.github.com>
2024-01-08 03:36:25 +0500

    Create index.js
    Expected life left in weeks months and days

---
+++ Life_in_weeks/index.js
@@ -0,0 +1,15 @@
+function lifeInWeeks(age) {
+    
+/************Don't change the code above************/    
+    
+    //Write your code here.
+    var years = 90 - age;
+    days = years * 365;
+    weeks = years * 52;
+    months = years * 12;
+    
+    
+/*************Don't change the code below**********/
+}
+
+lifeInWeeks(45);
