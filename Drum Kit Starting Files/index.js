bd77cd667eb7156f2d763a699f3087dd67a93320
Muhammad Moiz <97216206+Moiz005@users.noreply.github.com>
2024-01-13 21:24:29 +0500

    Delete Drum Kit Starting Files directory

--- Drum Kit Starting Files/index.js
+++
@@ -1,8 +0,0 @@
-var audio = new Audio("./sounds/tom-1.mp3");
-
-for(var i=0; i<document.querySelectorAll(".drum").length; i++){
-    document.querySelectorAll(".drum")[i].addEventListener("click", function (){
-        // audio.play();
-        this.style.color = "white";
-    });
-}
